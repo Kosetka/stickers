23ac1690665f4c21e8c7c109aae0fb3889436872
Mateusz <mateusz.zybura@gmailcom>
2018-07-16 11:00:17 +0200

    h

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 	session_start();
 	/* DATABASE CONFIGURATION */
-	define('DB_SERVER', 'localhost');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
+	define('DB_SERVER', '10.200.39.50');
+define('DB_USERNAME', 'stickers');
+define('DB_PASSWORD', '356413.stickers');
 	define('DB_DATABASE', 'stickers');
 	define('inc', 'true');
 	define('testVersion', 'false'); //false
